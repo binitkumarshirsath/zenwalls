a6fe5a7ea07e1d1799c4e7b21c1ce0a55e96549e
binitkumarshirsath <binitkumar0911@gmail.com>
2025-03-08 00:35:16 +0530

    add fav img icon

--- app/src/main/java/com/binit/zenwalls/ui/screens/wallpaper_list/components/ImageContainer.kt
+++ app/src/main/java/com/binit/zenwalls/ui/screens/wallpaper_list/components/ImageContainer.kt
@@ -1,11 +1,17 @@
 package com.binit.zenwalls.ui.screens.wallpaper_list.components
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -13,10 +19,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import coil3.compose.AsyncImagePainter
 import coil3.request.ImageRequest
@@ -50,6 +60,7 @@ fun ImageContainer(
         .build()
 
     Box(
+        contentAlignment = Alignment.BottomEnd,
         modifier = modifier
             .aspectRatio(aspectRatio)
             .clickable { onImageClick(image.id) }
@@ -71,18 +82,35 @@ fun ImageContainer(
         AsyncImage(
             model = imageRequest,
             onState = { state ->
-               coroutineScope.launch {
-                   delay(400)
-                   isLoading = if(state is AsyncImagePainter.State.Success){
-                       false
-                   }else{
-                       true
-                   }
-               }
+                coroutineScope.launch {
+                    delay(698)
+                    isLoading = if (state is AsyncImagePainter.State.Success) {
+                        false
+                    } else {
+                        true
+                    }
+                }
             },
             contentDescription = image.description,
             contentScale = ContentScale.Fit,
             modifier = Modifier.fillMaxSize()
         )
+        Box(
+            modifier
+                .size(25.dp)
+                .clip(RoundedCornerShape(50))
+                .background(
+                    Color.Red.copy(alpha = 0.3f),
+                )
+            ,
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                Icons.Default.Favorite,
+                contentDescription = "favourite_img",
+                modifier.size(20.dp),
+                tint = Color.White
+            )
+        }
     }
 }
