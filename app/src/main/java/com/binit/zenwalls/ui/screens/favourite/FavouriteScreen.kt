a4f735094ced00e67adabd27086a779daf83c9a4
binitkumarshirsath <binitkumar0911@gmail.com>
2025-03-08 17:52:27 +0530

    empty fav screen add

--- app/src/main/java/com/binit/zenwalls/ui/screens/favourite/FavouriteScreen.kt
+++ app/src/main/java/com/binit/zenwalls/ui/screens/favourite/FavouriteScreen.kt
@@ -1,17 +1,28 @@
 package com.binit.zenwalls.ui.screens.favourite
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,14 +30,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.binit.zenwalls.domain.model.UnsplashImage
 import com.binit.zenwalls.ui.components.BackButton
 import com.binit.zenwalls.ui.components.ImagePreview
+import com.binit.zenwalls.ui.navigation.Routes
 import com.binit.zenwalls.ui.screens.wallpaper_list.components.ImageContainer
+import com.binit.zenwalls.ui.theme.Primary
+import com.binit.zenwalls.ui.theme.Secondary
 import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -54,6 +71,13 @@ fun FavouriteScreen(
             Modifier.fillMaxSize()
         ) {
             TopAppBar(
+                colors = TopAppBarColors(
+                    containerColor = Primary,
+                    scrolledContainerColor = Primary,
+                    navigationIconContentColor = Color.White,
+                    titleContentColor = Color.White,
+                    actionIconContentColor = Color.White
+                ),
                 scrollBehavior = scrollBehavior,
                 title = {
                     Row {
@@ -63,6 +87,14 @@ fun FavouriteScreen(
                         )
                         Text("Hearts")
                     }
+                },
+                actions = {
+                    Icon(
+                        Icons.Default.Search, contentDescription = "search_icon",
+                        modifier
+                            .padding(end = 10.dp)
+                            .clickable { navcontroller.navigate(Routes.SearchScreen) }
+                    )
                 },
                 navigationIcon = {
                     Row {
@@ -76,11 +108,46 @@ fun FavouriteScreen(
                 },
             )
 
+            if (favouriteImages.itemCount == 0) {
+                Column(
+                    modifier
+                        .fillMaxSize()
+                        .background(Primary.copy(alpha = 0.9f)),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(Modifier.height(200.dp))
+                    Icon(
+                        Icons.Default.Favorite,
+                        contentDescription = "favourite_icon",
+                        modifier.size(80.dp),
+                        tint = Secondary.copy(
+                            alpha = 0.8f
+                        )
+                    )
+                    Spacer(Modifier.height(30.dp))
+                    Text(
+                        text = "No favourites yet",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.White
+                    )
+                    Spacer(Modifier.height(10.dp))
+                    Text(
+                        text = "Add some favourites to see them here",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Light,
+                        color = Color.White
+                    )
+
+                }
+            }
+
             LazyVerticalStaggeredGrid(
                 columns = StaggeredGridCells.Adaptive(150.dp),
                 userScrollEnabled = true,
                 modifier = modifier
-                    .fillMaxSize()
+                    .fillMaxSize().background(Primary.copy(alpha = 0.9f))
             ) {
                 items(count = favouriteImages.itemCount) {
                     favouriteImages[it]?.let { it1 ->
@@ -104,8 +171,6 @@ fun FavouriteScreen(
                     }
                 }
             }
-
-
 
 
         }
